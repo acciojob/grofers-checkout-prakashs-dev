07b994cd77ed50a5e81dd2eeba3819866314e38f
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-22 17:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
 const getSum = () => {
 //Add your code here
-  
+	    const getPrices = document.querySelectorAll(".price");
+        let sum = 0;
+        for (let i = 0; i < getPrices.length; i++) {
+          sum += parseInt(getPrices[i].innerText);
+        }
+        const table = document.querySelector("table");
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        cell1.innerText = "Total";
+        cell2.innerText = sum;
+	    getSumBtn.removeEventListener('click',getSum);
 };
 
 getSumBtn.addEventListener("click", getSum);
